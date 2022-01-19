794196816d1c3fbb206cb6a43e02cb281d95c527
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-18 22:49:45 -0300

    Rotas PUT e DELETE

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -15,7 +15,17 @@ router.post("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   console.log(req.params);
-  res.send(`ID: ${req.params.title}`);
+  res.send(`ID: ${req.params.id}`);
+});
+
+router.put("/:id", (req, res) => {
+  console.log(req.params);
+  res.send(`PUT ID: ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+  console.log(req.params);
+  res.send(`DELETE ID: ${req.params.id}`);
 });
 
 module.exports = router;
