08d94d176ddfb3e5f8a0e9b79a9430a57577265c
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-18 22:21:39 -0300

    Concentrando rotas

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const express = require("express");
 
-const app = express();
+const checklistRouter = require("./src/routes/checklist");
 
-//para usar o middleware
+const app = express();
 app.use(express.json());
 
-//middleware
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log(`Data: ${Date.now()}`);
-  next();
-};
-
-app.use(log);
-
-app.get("/", (req, res) => {
-  res.send("<h1>Minha lista de tarefas 🎯   </h1>");
-});
-
-app.get("/json", (req, res) => {
-  res.json({ title: "Tarefa", done: true });
-});
+app.use("/checklists", checklistRouter);
 
 app.listen(3000, () => {
   console.log("Servidor ativo! 🚀 ");
