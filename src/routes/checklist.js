82f2f9bf70037ac9dfcb4e669b26f8e611ea1e5d
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2022-01-18 22:43:24 -0300

    Parâmetros nas rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { route } = require("express/lib/application");
 
 const router = express.Router();
 
@@ -10,6 +11,11 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
   console.log(req.body);
   res.status(200).send(req.body);
+});
+
+router.get("/:id", (req, res) => {
+  console.log(req.params);
+  res.send(`ID: ${req.params.title}`);
 });
 
 module.exports = router;
